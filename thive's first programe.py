c927373efa8ec8e23737678d24e1af59e06d4f54
Thiveyamahilan <134792729+Thiveyamahilan@users.noreply.github.com>
2023-05-30 09:45:25 +0530

    Add files via upload

---
+++ thive's first programe.py
@@ -0,0 +1,10 @@
+print('hallo,sir')
+person=input('what is your name?')
+print('hallo',person)
+
+age=input('how old are you ?')
+print('you are',age,'old')
+
+weight=input('what is your weight?')
+hight=input('what is your hight?')
+print('your BMI is',weight, divide ,hight, into ,hight,)
